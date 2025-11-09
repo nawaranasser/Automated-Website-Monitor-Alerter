0886163fb0dc54105717d5c05f1c2767bd3364ae
nora nasser eldin <125719941+nawaranasser@users.noreply.github.com>
2025-11-09 23:25:04 +0200

    Obscure sensitive Gmail configuration details
    Updated Gmail sender and app password for security.

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 # Gmail Configuration
-GMAIL_SENDER = "noranasser750@gmail.com"  # Your Gmail address
-GMAIL_APP_PASSWORD = "tdkz nkju rcwp xrix"  # The app password you generated
+GMAIL_SENDER = "nora*********@gmail.com"  # Your Gmail address
+GMAIL_APP_PASSWORD = "your_16_digit_app_password" # Search how to generate The app password
 RECIPIENT_EMAIL = "noranasser750@gmail.com"  # Where to send alerts (can be same as sender)
 
 # Monitor Configuration
